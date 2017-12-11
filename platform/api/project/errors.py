01d5c6124b2d5908cb5779f7ee17cf67815830c8
v.bakin <mixkorshun@gmail.com>
2017-12-12 01:27:01 +0300

    add default error handler for 400 exceptions

--- platform/api/project/errors.py
+++ platform/api/project/errors.py
@@ -1,11 +1,12 @@
 from flask import jsonify
-from werkzeug.exceptions import MethodNotAllowed, NotFound, Forbidden
+from werkzeug.exceptions import MethodNotAllowed, NotFound, Forbidden, \
+    BadRequest
 
 from . import app
 
 
 @app.errorhandler(NotFound)
-def handle_method_not_allowed(error):
+def handle_not_found(error):
     response = jsonify({
         'error_code': 'not_found',
         'error_message': error.description
@@ -14,8 +15,18 @@ def handle_method_not_allowed(error):
     return response
 
 
+@app.errorhandler(BadRequest)
+def handle_bad_request(error):
+    response = jsonify({
+        'error_code': 'bad_request',
+        'error_message': error.description
+    })
+    response.status_code = error.code
+    return response
+
+
 @app.errorhandler(Forbidden)
-def handle_method_not_allowed(error):
+def handle_forbidden(error):
     response = jsonify({
         'error_code': 'not_allowed',
         'error_message': error.description
